d479c2d477d3acaa345eee1e655cfd6f1e96c0ad
VitoriaPereiraKlapowsko <vitoriapereiraklapowsko@gmail.com>
2024-03-03 17:07:04 -0300

    Arrumando a duplicação de código com o uso da Herança

--- modelo.py
+++ modelo.py
@@ -1,46 +1,37 @@
-class Filme:
-    def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title() #Title deixa todas as 1° letras em maiusculo
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title() #Title deixa todas as 1° letras em maiusculo
         self.ano = ano
-        self.duracao = duracao
-        self.__likes = 0
+        self._likes = 0
       
     @property  
     def likes(self):
-        return self.__likes
+        return self._likes
     
     def dar_like(self):
-        self.__likes += 1 # Adiciona 1 na minha contagem de likes
+        self._likes += 1 # Adiciona 1 na minha contagem de likes
         
     @property
     def nome(self):
-        return self.__nome
-    
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-    
-class Serie:
-    def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
-        self.temporadas = temporadas
-        self.__likes = 0
-        
-    @property #Usando os atributos privados
-    def likes(self):
-        return self.__likes
-        
-    def dar_like(self):
-        self.__likes += 1
-        
-    @property
-    def nome(self):
-        return self.__nome
+        return self._nome
     
     @nome.setter #Definindo um setter para pegar valores, no caso aqui estou pegando o valor do nome
     def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
+        self._nome = novo_nome.title()
+        
+class Filme(Programa): #Herdando informações da classe Programa
+    def __init__(self, nome, ano, duracao):
+        self._nome = nome.title() 
+        self.ano = ano
+        self.duracao = duracao
+        self._likes = 0
+    
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        self._nome = nome.title()
+        self.ano = ano
+        self.temporadas = temporadas
+        self._likes = 0
 
 vingadores = Filme('vigadores - guerra infinita', 2018, 160)
 vingadores.dar_like()

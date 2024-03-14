70c41058c7842ca003e5ae066dafc94102bbe119
VitoriaPereiraKlapowsko <vitoriapereiraklapowsko@gmail.com>
2024-03-14 16:23:37 -0300

    Finalizando o projeto

--- modelo.py
+++ modelo.py
@@ -37,14 +37,44 @@ class Serie(Programa):
         
     def __str__(self):
         return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes'
+   
+   
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+        
+    def __getitem__(self, item):
+        return self._programas[item]
        
+    @property
+    def listagem(self):
+        return self._programas
+    
+    def __len__(self):
+        return len(self._programas)
+    
 vingadores = Filme('vigadores - guerra infinita', 2018, 160)
-vingadores.dar_like()
 atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+vingadores.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+atlanta.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
 
-filmes_e_series = [vingadores, atlanta]
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-for programa in filmes_e_series:
+print(f'Tamnnho da Playlist: {len(playlist_fim_de_semana)}')
+print(playlist_fim_de_semana[0])
+
+for programa in playlist_fim_de_semana:
     print(programa)
+    
